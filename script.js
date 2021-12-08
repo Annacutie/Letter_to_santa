2b52b77b48d9b3342d7a986b1a9f7d70b1e5eb00
Annacutie <ikhrist@gmail.com>
2021-12-08 16:29:59 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const DB_name='lLetter_to_santa';
+constb_version=1;
+let name=Document.querySelector('#name').value;
+let city=Document.querySelector('#city').value;
+const storage_name='user_data';
+let gift_wish=Document.querySelector('#gift_wish').value;
