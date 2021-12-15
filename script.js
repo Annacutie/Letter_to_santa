0a084619ae92c4d8b3bfe1aa9afeff651069242e
Annacutie <ikhrist@gmail.com>
2021-12-15 15:36:00 +0300

    I updated my js file

--- script.js
+++ script.js
@@ -1,6 +1,49 @@
-const DB_name='lLetter_to_santa';
-constb_version=1;
-let name=Document.querySelector('#name').value;
-let city=Document.querySelector('#city').value;
+var db;
+const DB_name="Letter_to_santa";
+const db_version=5;
 const storage_name='user_data';
-let gift_wish=Document.querySelector('#gift_wish').value;
+document.getElementById("save").addEventListener("click", saveData);
+let openRequest = indexedDB.open(DB_name, db_version);
+openRequest.addEventListener('upgradeneeded',(event)=> {
+    db=event.target.result;
+    let oldVersion=event.oldVersion
+    let newVersion=event.newVersion
+    console.log(`Upgrade from ${oldVersion} to ${newVersion}`);
+    if (!db.objectStoreNames.contains(storage_name)){
+        let dataStore = db.createObjectStore(storage_name, {keyPath:'id', autoIncrement:true});
+    }
+})
+
+openRequest.addEventListener('sucssess',(event)=>{
+    db=event.target.result;
+    console.log('success');
+})
+
+openRequest.addEventListener('error',(error)=>{
+    console.warn(error)}) 
+
+function saveData () {
+    let name=document.querySelector("#name").value;
+    let city=document.querySelector('#city').value;
+    let gift_wish=document.querySelector('#gift_wish').value; 
+  
+let letterData={name, city, gift_wish}
+  
+let tx=db.transaction(storegeName, 'readwrite');
+tx.oncomplete=(event)=>{
+    console.log(event)   
+}
+
+tx.onerror=(error)=>{
+    console.warn(error)   
+}
+let store=tx.objectStore(storage_name);
+let request=store.add(letterData);
+
+request.onsuccess=(event)=>{
+    console.log('Successful add')   
+}
+
+requset.onerror=(error)=>{
+    console.warn(error)   
+}}
